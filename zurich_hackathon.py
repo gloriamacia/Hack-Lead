d7e77b97df1ff4a5fb6a88b0d738c81689def1e9
palas22 <laurapalacio94@gmail.com>
2018-06-17 10:20:45 +0100

    plot confusion matrix done

--- zurich_hackathon.py
+++ zurich_hackathon.py
@@ -7,6 +7,9 @@ from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense # for hidden layers
 from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
 
 """Load data"""
 data = pd.read_excel("zurich_insurance.xlsx")
@@ -81,20 +84,54 @@ def baseline_model():
     classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
     return classifier
 
-classifier = KerasClassifier(build_fn = baseline_model, epochs = 10, batch_size = 10)
-kfold = KFold(n_splits=5, shuffle=True, random_state=0)
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv=kfold)
+classifier = KerasClassifier(build_fn = baseline_model, epochs = 2, batch_size = 10)
+kfold = KFold(n_splits = 5, shuffle = True, random_state = 0)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = kfold)
 mean = accuracies.mean()
 std = accuracies.std()
 print("Baseline: %.2f%% (%.2f%%)" % (mean*100, std*100))
 
 # Part 3 - Making predictions and evaluating the model
 # Predicting the Test set results
-classifier.fit(X_train, y_train, batch_size=10, epochs=10)
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 2)
 y_pred = classifier.predict(X_test)
+
 # Making the Confusion Matrix
-cm = confusion_matrix(y_test.argmax(axis=1), y_pred)
-#esb = confusion_matrix(y_test.argmax(axis=1), y_pred.round().argmax(axis=1))
+cm = confusion_matrix(y_test.argmax(axis = 1), y_pred)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize = False,
+                          title = 'Confusion matrix',
+                          cmap = plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation = 45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment = "center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+plt.figure()
+plot_confusion_matrix(cm, classes = ['A','B','C','D'], normalize = True,
+                      title = 'Normalized confusion matrix')
+plt.show()
 
 #from sklearn.externals import joblib
 ## save the model to disk
@@ -107,7 +144,3 @@ cm = confusion_matrix(y_test.argmax(axis=1), y_pred)
 #loaded_model = joblib.load(filename)
 #result = loaded_model.score(X_test, y_test)
 #print(result)
-
-import numpy
-numpy.unique(y_pred)
-
